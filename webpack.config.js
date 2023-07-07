6cdf41f08b20a98634ef2b3c635c5a9e95e0a006
tasnimchy96 <tasnim.achy96@gmail.com>
2023-07-07 15:11:14 +0600

    Add interactive to-do-list

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
+    index: './src/index.js',
+    print: './src/print.js',
   },
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
-  mode: "development",
+  mode: 'development',
 };
